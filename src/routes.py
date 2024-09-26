330b5b9540a49dc1e4a3caa7517e4ae620a845b0
pabloemanuelpbl <pablo.fju.pa@gmail.com>
2024-09-26 10:39:43 -0300

    refactor: moving Matheus's html into the project

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "ola"
+    return render_template("home.html")
--- src/server.py
+++ src/server.py
@@ -1,4 +1,13 @@
+import os
+from dotenv import load_dotenv
 from application import app
 
+load_dotenv()
+
+SERVER_PORT = os.getenv("SERVER_PORT")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run( 
+        debug=True,
+        port=SERVER_PORT
+    )
