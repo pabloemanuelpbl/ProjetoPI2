9fb9d3864a6b2528914941e111d32227554e4279
pabloemanuelpbl <pablo.fju.pa@gmail.com>
2024-10-04 10:38:22 -0300

    feat: adding new pages, 'about' and 'services'

--- src/routes.py
+++ src/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
